b40efc87b35c8850350361e0072f0a2155bf780c
DariaSmeshnowa <smeshnowa.daria@gmail.com>
2025-12-10 15:02:47 +0300

    add filters for table with all cnv

---
+++ src/main/groovy/operations/filters/CNVALLFilter.groovy
@@ -0,0 +1,20 @@
+package operations
+
+import com.developmentontheedge.be5.groovy.GDynamicPropertySetSupport
+import com.developmentontheedge.be5.server.operations.FilterOperation
+
+class CNVFilter extends FilterOperation
+{
+    @Override
+    GDynamicPropertySetSupport getFilterParameters(Map<String, Object> presetValues) throws Exception
+    {
+        def dps = new GDynamicPropertySetSupport()
+        
+        dps.gene = [ DISPLAY_NAME: "Involved gene", TYPE: String ]
+        dps.pat_status  = [ DISPLAY_NAME: "Pathogenicity", TYPE: String,
+                     TAG_LIST_ATTR: queries.getTagsFromCustomSelectionView("cnvsall", "PathogenicitySelectionView"),
+                     RELOAD_ON_CHANGE: true ]
+
+        return dps
+    }
+}
