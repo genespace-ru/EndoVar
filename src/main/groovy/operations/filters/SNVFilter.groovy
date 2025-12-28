6c5478485ddff10358e458d7f752e8353769f6dc
DariaSmeshnowa <smeshnowa.daria@gmail.com>
2025-12-28 13:35:16 +0300

    add itogsnvs with patients and diagnosis

--- src/main/groovy/operations/filters/SNVFilter.groovy
+++ src/main/groovy/operations/filters/SNVFilter.groovy
@@ -13,8 +13,9 @@ class SNVFilter extends FilterOperation
         dps.gene = [ DISPLAY_NAME: "Gene", TYPE: String ]
         dps.Variant = [ DISPLAY_NAME: "Variant", TYPE: String ]
         dps.pa  = [ DISPLAY_NAME: "Pathogenicity", TYPE: String,
-                     TAG_LIST_ATTR: queries.getTagsFromCustomSelectionView("tiers", "PathogenicitySelectionView"),
-                     RELOAD_ON_CHANGE: true ]                                    
+                     TAG_LIST_ATTR: queries.getTagsFromCustomSelectionView("snvspats", "PathogenicitySelectionView"),
+                     RELOAD_ON_CHANGE: true ]    
+        dps.Diagnosis = [ DISPLAY_NAME: "Patient diagnosis", TYPE: String ]                                                     
         return dps
     }
 }
